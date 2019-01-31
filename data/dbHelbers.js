8a8c3255e1aceeff6b2a4df6c854fc44f94f5ace
Justin <jdlow2@gmail.com>
2019-01-31 17:39:40 -0500

    helper tests for db deletion

--- data/dbHelbers.js
+++ data/dbHelbers.js
@@ -3,4 +3,5 @@ const db = require('./dbConfig');
 module.exports = {
   addPerson: person => db('peoples').insert(person),
   getPeople: () => db('peoples'),
+  deletePerson: id => db('peoples').where({ id }).delete(),
 }
--- data/dbHelbers.test.js
+++ data/dbHelbers.test.js
@@ -5,10 +5,21 @@ afterEach(async () => {
   await db('peoples').truncate();
 });
 
-describe.skip('peoples helper', () => {
-  it('should insert a person and return a person', async () => {
+describe('peoples helper', () => {
+  it.skip('should insert a person and return a person', async () => {
     const person = await helpers.addPerson({first_name: 'justin', last_name: 'lowry'});
     const people = await helpers.getPeople();
     expect(people).toHaveLength(1);
+  });
+  it('should delete a person with a given id', async () => {
+    await db('peoples').truncate();
+    const [ id ] = await helpers.addPerson({first_name: 'justin', last_name: 'lowry'});
+    let people = await helpers.getPeople();
+    expect(people).toHaveLength(1);
+    await helpers.deletePerson(id);
+    people = await helpers.getPeople();
+    expect(people).toHaveLength(0);
+    
   })
 })
+
